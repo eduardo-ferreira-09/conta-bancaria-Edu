983f448b7637f1bbeea2989b02ec616e74df7c30
Eduardo Ferreira <eduzinobrabo655@gmail.com>
2026-03-18 16:50:39 -0300

    atualização 1803

--- src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
+++ src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
@@ -3,7 +3,10 @@ package com.senai.conta_bancaria.application.service;
 import com.senai.conta_bancaria.domain.entity.Usuario;
 import com.senai.conta_bancaria.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -13,7 +16,30 @@ public class UsuarioService {
     UsuarioRepository usuarioRepository;
 
     public Usuario cadastrarUsuario(Usuario usuario) {
-        usuarioRepository.save(usuario);
-        return usuario;
+
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario usuario) {
+        Usuario usuarioAtualizado = buscarUsuarioPorId(id);
+        if (usuarioAtualizado != null) {
+            usuarioAtualizado.setNome(usuario.getNome());
+            usuarioAtualizado.setEmail(usuario.getEmail());
+            usuarioAtualizado.setSenha(usuario.getSenha());
+            return usuarioRepository.save(usuarioAtualizado);
+
+        }
+        return null;
+    }
+
+    public void deletarUsuario(Long id) {
+        usuarioRepository.deletarUsuario(id);
+    }
+
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
     }
 }
--- src/main/java/com/senai/conta_bancaria/interface_ui/controller/UsuarioController.java
+++ src/main/java/com/senai/conta_bancaria/interface_ui/controller/UsuarioController.java
@@ -4,10 +4,12 @@ package com.senai.conta_bancaria.interface_ui.controller;
 import com.senai.conta_bancaria.application.service.UsuarioService;
 import com.senai.conta_bancaria.domain.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -16,9 +18,32 @@ public class UsuarioController {
     UsuarioService usuarioService;
 
     @PostMapping
-    public Usuario cadastrarUsuario(@RequestBody Usuario usuario){
-
-            return  usuarioService.cadastrarUsuario(usuario);
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioCadastrado = usuarioService.cadastrarUsuario(usuario);
+        return ResponseEntity.created(
+        URI.create("/usuario/" + usuarioCadastrado.getId())
+                ).body(usuarioCadastrado)
     }
+@GetMapping
+public ResponseEntity<List<Usuario>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
 
 }
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorId(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, usuario));
+
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return new ResponseEntity.noContent().build();
+    }
+}
+
+
